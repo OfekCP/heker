cf7a7a8f3f89f16708a5a3c2ab0c858ebcc98d35
OfekCP <ofekzvulunov@gmail.com>
2024-05-03 20:15:26 +0300

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,15 +11,16 @@ const Home = () => {
             </div>
             <div className="video-container">
                 <video autoPlay muted playsInline className='logo-video'>
-                    <source src="./src/Solid Logo Reveal_free (1).mp4_1714254924321.mp4" type="video/mp4" />
+                    <source src="/src/Solid Logo Reveal_free (1).mp4_1714254924321.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
+
             </div>
             <div className='random-works'>
                 <span>דוגמאות לעבודות שבוצעו על ידינו:</span>
                 <div className='item'>
-                <ComparisonSlider before={'https://cdn.langeek.co/photo/26055/original/before'} after={'https://cdn.langeek.co/photo/26056/original/after'} />
-                <ComparisonSlider before={'https://cdn-icons-png.flaticon.com/512/6702/6702465.png'} after={'https://upload.wikimedia.org/wikipedia/en/2/23/After_film_series_logo.png'} />
+                    <ComparisonSlider before={'https://cdn.langeek.co/photo/26055/original/before'} after={'https://cdn.langeek.co/photo/26056/original/after'} />
+                    <ComparisonSlider before={'https://cdn-icons-png.flaticon.com/512/6702/6702465.png'} after={'https://upload.wikimedia.org/wikipedia/en/2/23/After_film_series_logo.png'} />
                 </div>
             </div>
         </div>
